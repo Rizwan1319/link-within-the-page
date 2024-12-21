bf07ac0a0fb6d3a23154b04429af66f83dff8e63
Rizwan1319 <rizwanmeeran13@gmail.com>
2024-12-21 10:36:38 +0530

    fix:remove-unwanted

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  plugins: [
+    require('tailwindcss-animated')
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
